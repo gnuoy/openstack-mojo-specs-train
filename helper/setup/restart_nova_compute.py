e37088d728202a0b08eae28deadddc3569801f91
Liam Young <liam.young@canonical.com>
2019-10-20 09:21:59 +0000

    Collection of fixes

--- helper/setup/restart_nova_compute.py
+++ helper/setup/restart_nova_compute.py
@@ -11,9 +11,12 @@ from zaza.openstack.utilities import (
 
 def main(argv):
     zaza.openstack.utilities.cli.setup_logging()
-    logging.info('Restarting nova compute')
+    logging.info('Restarting nova compute and scheduler')
     for unit in zaza.model.get_units('nova-compute'):
         zaza.model.run_on_unit(unit.name, 'systemctl restart nova-compute')
+        logging.info('Restarted on {}'.format(unit.name))
+    for unit in zaza.model.get_units('nova-cloud-controller'):
+        zaza.model.run_on_unit(unit.name, 'systemctl restart nova-scheduler')
         logging.info('Restarted on {}'.format(unit.name))
 
 if __name__ == "__main__":
